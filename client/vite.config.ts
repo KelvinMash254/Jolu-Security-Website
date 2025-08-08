8e675a9a205bc335157f188b2323d3028282f75d
KelvinMash254 <kelvinmacharia@gmail.com>
2025-08-08 18:52:08 +0300

    vite config update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/Jolu-Security-Website/",
   server: {
     host: "::", // IPv6 support
     port: 8080,
